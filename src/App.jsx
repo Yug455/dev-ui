0180fe2cb0d1257034bca9c5139982841106e827
yug455 <yugmalhotr91@gmail.com>
2026-01-16 01:14:23 +0530

    reactrouterdom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 
-import { useState } from "react"
+import { Profiler, useState } from "react"
+import Navbar from "./Navbar"
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Body from "./body";
+import Login  from "./Login";
+import Profile from "./Profile";
 function App() {
   const [count, setCount] = useState(0)
 
   return (
   <>
-  <h1>hello world</h1>
+    <BrowserRouter basename="/">
+    <Routes>
+      <Route path="/" element={<Body/>}>
+      <Route path="/Login" element={<Login></Login>}> </Route>
+      <Route path="/Profile" element={<Profile></Profile>}></Route>
+      </Route>
+    </Routes>
+    </BrowserRouter>
+    
   </>
   )
 }
---
+++ src/Login.jsx
@@ -0,0 +1,4 @@
+const Login=()=>{
+ return <div>Hello after login</div>
+}
+export default Login;
